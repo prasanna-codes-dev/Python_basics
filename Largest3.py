80c72947e1251e1d7a2b301e0829f616e5a03f08
prasanna-codes-dev <prasannalakkimsetty11@gmail.com>
2026-04-13 23:02:55 +0530

    Create Largest3.py

---
+++ Largest3.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+if a > b and a > c:
+    print("a is largest")
+elif b > c:
+    print("b is largest")
+else:
+    print("c is largest")
