8c61c11708c348d78b148daecfe81f2ba9320724
prasanna-codes-dev <prasannalakkimsetty11@gmail.com>
2026-07-16 16:33:03 +0530

    Added Duck Number program

---
+++ duck_number.py
@@ -0,0 +1,10 @@
+num = input("Enter a number: ")
+
+if num[0] == '0':
+    print(num, "is Not a Duck Number")
+
+elif '0' in num:
+    print(num, "is a Duck Number")
+
+else:
+    print(num, "is Not a Duck Number")
