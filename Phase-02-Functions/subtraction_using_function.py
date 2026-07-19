3b4929d118be9ee43fb7b7d3d0e722ed57ae69b3
prasanna-codes-dev <prasannalakkimsetty11@gmail.com>
2026-07-19 08:38:35 +0530

    Added Subtraction Using Function program

---
+++ Phase-02-Functions/subtraction_using_function.py
@@ -0,0 +1,9 @@
+def subtraction(a, b):
+    return a - b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = subtraction(num1, num2)
+
+print("Difference =", result)
