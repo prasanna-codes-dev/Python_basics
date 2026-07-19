d7f34f2a69021dea90d5f7162854741844106aa9
prasanna-codes-dev <prasannalakkimsetty11@gmail.com>
2026-07-19 08:39:29 +0530

    Added Multiplication Using Function program

---
+++ Phase-02-Functions/multiplication_using_function.py
@@ -0,0 +1,9 @@
+def multiplication(a, b):
+    return a * b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = multiplication(num1, num2)
+
+print("Product =", result)
