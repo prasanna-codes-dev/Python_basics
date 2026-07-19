4ed41bcdd27f7daddaeaa7d68924da143d27494d
prasanna-codes-dev <prasannalakkimsetty11@gmail.com>
2026-07-19 08:40:41 +0530

    Added Division Using Function program

---
+++ Phase-02-Functions/division_using_function.py
@@ -0,0 +1,9 @@
+def division(a, b):
+    return a / b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = division(num1, num2)
+
+print("Quotient =", result)
